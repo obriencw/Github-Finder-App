e1bf281e58e96fbd465bd55a78ed9ed16d79f01b
Koskie <pwdrkng@yahoo.com>
2022-06-03 08:35:20 -0500

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <>
+        <UserResults />
+      
+    </>
+  )
+}
+
+export default Home
